b3f9f3670cc086b732257f17b259b8a10c5757f9
Mike <mahathaway93@gmail.com>
2017-03-30 10:12:41 -0700

    added an error handler

--- script.js
+++ script.js
@@ -3,13 +3,44 @@ const app = express()
 const houses = require('./house_data.js')
 const members = require('./member_data.js')
 
+const allMembers = (req,res) => res.send(members)
+const allHouses = (req,res) => res.send(houses)
+
+const specificMembers = (req,res) => {
+  const index = req.params.index
+  res.send(members[index])
+}
+
+const specificHouses = (req,res,next) => {
+  const index = req.params.index
+  const error = {status: 404, message: 'ya done fucked up'}
+
+  if(houses[index]){
+    res.send(houses[index])
+  }
+  next(error)
+}
+
+const errorHandler = (err,req,res,next) => {
+  console.log('we in the errors yo',err)
+  res.status(err.status).json(err)
+}
+
 app.get('/',(req,res) => {
   res.send('please select /houses or /members')
 })
 
-app.get('/members',(req,res) => {
-  res.send(members)
-})
+app.get('/members',allMembers)
+
+app.get('/houses',allHouses)
+
+app.get('/members/:index',specificMembers)
+
+app.get('/houses/:index',specificHouses)
+
+app.use(errorHandler)
+
+
 
 app.listen(8000,(req,res) => {
   console.log('We\'re family friendly')
