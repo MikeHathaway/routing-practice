7045ef17bcf0f0b7211b92db07964b050649f753
Mike <mahathaway93@gmail.com>
2017-03-30 09:43:02 -0700

    added some bad world leaders

---
+++ member_data.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    first_name: "Richard",
+    last_name: "Nixon",
+    attendance_number: 37,
+    id: 666
+  },
+  {
+    first_name: "Idi",
+    last_name: "Amin",
+    attendance_number: 5,
+    id: 47
+  },
+  {
+    first_name: "Josef",
+    last_name: "Stalin",
+    attendance_number: 2,
+    id: 82
+  }
+]
