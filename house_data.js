13399b631b20c3e235f133840f2e0cd68efe3f8d
Ian Harvey <ith.harvey@gmail.com>
2017-03-30 09:41:52 -0700

    added house_data.js

---
+++ house_data.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+      house: 'Lovelace',
+      color: 'purple',
+      symbol: 'prariedog',
+      mascot: 'dragon',
+      id: 69
+  },
+  {
+    house: 'Liskov',
+    color: 'yellow',
+    symbol: 'acoin',
+    mascot: 'dove',
+    id: 100
+  },
+  {
+    house: 'Turner',
+    color: 'blue',
+    symbol: 'highfive',
+    mascot: 'cowboy',
+    id: 10
+  },
+  {
+  house: 'Ellis',
+  color: 'red',
+  symbol: 'the letter s',
+  mascot: 'turtle',
+  id: 13
+  },   
+
+]
