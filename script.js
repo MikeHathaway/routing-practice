497e52abfd33b6cb67c0b041865c8479c4862eda
Ian Harvey <ith.harvey@gmail.com>
2017-03-30 09:54:20 -0700

    added in our module.exports

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const houses = require('./house_data.js')
+const members = require('./member_data.js')
 
 app.get('/',(req,res) => {
-  res.send('Hello WORLD')
+  res.send('please select /houses or /members')
+})
+
+app.get('/members',(req,res) => {
+  res.send(members)
 })
 
 app.listen(8000,(req,res) => {
