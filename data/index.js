c514a94d728526061db1f057de0c4a0d9d90bd68
Mike <mahathaway93@gmail.com>
2017-03-30 10:53:22 -0700

    upgraded logic with closures

---
+++ data/index.js
@@ -0,0 +1,7 @@
+houseData = require('./house_data.js')
+memberData = require('./member_data.js')
+
+module.exports = {
+  houses: houseData,
+  members: memberData
+}
--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 const express = require('express')
 const app = express()
-const houses = require('./house_data.js')
-const members = require('./member_data.js')
+const port = process.env.PORT || 8000
+const data = require('./data')
 
-const allMembers = (req,res) => res.send(members)
-const allHouses = (req,res) => res.send(houses)
-
-const specificMembers = (req,res,next) => {
-  const index = req.params.index
-  const error = {status: 404, message: 'ya done fucked up'}
-  members[index] ? res.send(members[index]) : next(error)
+//Generalized response logic
+const getResource = (resource) => {
+  return (req,res) => {
+    res.send(data[resource])
+  }
 }
 
-const specificHouses = (req,res,next) => {
-  const index = req.params.index
-  const error = {status: 404, message: 'ya done fucked up'}
-  houses[index] ? res.send(houses[index]) : next(error)
-  }
+const getSpecificResource = (resource) => {
+  const error = {status: 404, message: 'ERROR'}
 
+  return (req,res,next) => {
+    const index = req.params.index
+    const dataSet = data[resource][index]
+    dataSet ? res.send(dataSet) : next(error)
+  }
+}
 
 const errorHandler = (err,req,res,next) => {
-  console.log('we in the errors yo',err)
   res.status(err.status).json(err)
 }
+
 
 app.get('/',(req,res) => {
   res.send('please select /houses or /members')
 })
 
+app.post('/members/:index',(req,res) => {})
 
-app.get('/members',allMembers)
-app.get('/houses',allHouses)
-app.get('/members/:index',specificMembers)
-app.get('/houses/:index',specificHouses)
+app.get('/members',getResource('members'))
+app.get('/houses',getResource('houses'))
+
+app.get('/members/:index',getSpecificResource('members'))
+app.get('/houses/:index',getSpecificResource('houses'))
+
 app.use(errorHandler)
 
 
-
-app.listen(8000,(req,res) => {
+app.listen(port,(req,res) => {
   console.log('We\'re family friendly')
 })
